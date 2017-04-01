5e605a39ae52e69e9603b9294526f7421decfe1a
dragondmii <robert.c.epstein@gmail.com>
2017-04-01 10:22:55 -0600

    creating

---
+++ BSTree.py
@@ -0,0 +1,106 @@
+
+#############################
+# module: BSTree.py
+# description: binary search tree
+# bugs to vladimir dot kulyukin at usu dot edu
+#############################
+
+from BSTNode import BSTNode
+
+class BSTree:
+
+    def __init__(self, root=None):
+        self.__root = root
+        if root==None:
+            self.__numNodes = 0
+        else:
+            self.__numNodes = 1
+
+    def getRoot(self):
+        return self.__root
+
+    def getNumNodes(self):
+        return self.__numNodes
+
+    def isEmpty(self):
+        return self.__root == None
+
+    def hasKey(self, key):
+        if self.isEmpty():
+            return False
+        else:
+            currNode = self.__root
+            while currNode != None:
+                if currNode.getKey() == key:
+                    return True
+                elif key < currNode.getKey():
+                    currNode = currNode.getLeftChild()
+                elif key > currNode.getKey():
+                    currNode = currNode.getRightChild()
+                else:
+                    raise Exception('hasKey: ' + str(key))
+            return False
+
+    def insertKey(self, key):
+        if self.isEmpty():
+            self.__root = BSTNode(key=key)
+            self.__numNodes += 1
+            return True
+        elif self.hasKey(key):
+            return False
+        else:
+            currNode = self.__root
+            parNode = None
+            while currNode != None:
+                parNode = currNode
+                if key < currNode.getKey():
+                    currNode = currNode.getLeftChild()
+                elif key > currNode.getKey():
+                    currNode = currNode.getRightChild()
+                else:
+                    raise Exception('insertKey: ' + str(key))
+            if parNode != None:
+                if key < parNode.getKey():
+                    parNode.setLeftChild(BSTNode(key=key))
+                    self.__numNodes += 1
+                    return True
+                elif key > parNode.getKey():
+                    parNode.setRightChild(BSTNode(key=key))
+                    self.__numNodes += 1
+                    return True
+                else:
+                    raise Exception('insertKey: ' + str(key))
+            else:
+                raise Exception('insertKey: parNode=None; key= ' + str(key))
+     
+    def heightOf(self):
+        ## your code
+	pass
+
+    def isBalanced(self):
+        ## your code
+	pass
+
+    def __displayInOrder(self, currnode):
+        if currnode == None:
+            print('NULL')
+        else:
+            self.__displayInOrder(currnode.getLeftChild())
+            print(str(currnode))
+            self.__displayInOrder(currnode.getRightChild())
+
+    def displayInOrder(self):
+        self.__displayInOrder(self.__root)
+
+    def isList(self):
+        ## your code 
+	pass
+
+                
+
+
+    
+            
+
+    
+   
---
+++ rand_bst.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+###############################################
+# module: rand_bst.py
+# description: starter code for HW09
+# bugs to vladimir dot kulyukin at usu dot edu
+###############################################
+
+from BSTNode import BSTNode
+from BSTree import BSTree
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+def gen_rand_bst(num_nodes, a, b):
+    ## your code
+    pass
+
+def estimate_list_prob_in_rand_bsts_with_num_nodes(num_trees, num_nodes, a, b):
+    ## your code
+    pass
+
+def estimate_list_probs_in_rand_bsts(num_nodes_start, num_nodes_end, num_trees, a, b):
+    d = {}
+    for num_nodes in xrange(num_nodes_start, num_nodes_end+1):
+        d[num_nodes] = estimate_list_prob_in_rand_bsts_with_num_nodes(num_trees, num_nodes, a, b)
+    return d
+
+def estimate_balance_prob_in_rand_bsts_with_num_nodes(num_trees, num_nodes, a, b):
+    ## your code
+    pass
+
+def estimate_balance_probs_in_rand_bsts(num_nodes_start, num_nodes_end, num_trees, a, b):
+    d = {}
+    for num_nodes in xrange(num_nodes_start, num_nodes_end+1):
+        d[num_nodes] = estimate_balance_prob_in_rand_bsts_with_num_nodes(num_trees, num_nodes, a, b)
+    return d
+
+def plot_rbst_lin_probs(num_nodes_start, num_nodes_end, num_trees):
+    ## your code
+    pass
+
+def plot_rbst_balance_probs(num_nodes_start, num_nodes_end, num_trees):
+    ## your code
+    pass
+
+### ========== UNIT TESTS =============
+
+## unit_test_01 tests BSTNode constructor
+##           5
+##          /  \
+##         3    10
+def unit_test_01():
+    r = BSTNode(key=5)
+    lc = BSTNode(key=3)
+    rc = BSTNode(key=10)
+    print('root=%s, lc=%s, rc=%s' % (r, lc, rc))
+    r.setLeftChild(lc)
+    r.setRightChild(rc)
+    assert ( r.getLeftChild().getKey()   == 3 )
+    assert ( r.getRightChild().getKey() == 10 )
+
+## unit_test_01() contstructs two bst's.
+## bst
+##        10
+##       /  \
+##      3   20
+##
+## bst2
+##        5
+##       /
+##     3
+##       \
+##        4
+def unit_test_02():
+    bst = BSTree()
+    bst.insertKey(10)
+    bst.insertKey(3)
+    bst.insertKey(20)
+    assert ( bst.isBalanced() )
+    assert ( bst.heightOf() == 1 )
+    assert ( not bst.isList() )
+    print('displaying bst')
+    bst.displayInOrder()
+    print('-------')
+
+    bst2 = BSTree()
+    bst2.insertKey(5)
+    bst2.insertKey(3)
+    bst2.insertKey(4)
+    assert ( not bst2.isBalanced() )
+    assert ( bst2.heightOf() == 2 )
+    assert ( bst2.isList() )
+    print('displaying bst2')
+    bst2.displayInOrder()
+
+def unit_test_03():
+    rbst = gen_rand_bst(5, 0, 10)
+    print('root=' + str(rbst.getRoot()))
+    rbst.displayInOrder()
+    print('is list? = ' + str(rbst.isList()))
+    print('height = ' + str(rbst.heightOf()))
+    print('is bal? = ' + str(rbst.isBalanced()))
+
+def unit_test_04():
+    print(estimate_list_prob_in_rand_bsts_with_num_nodes(100, 5, 0, 1000))
+
+def unit_test_05():
+    d = estimate_list_probs_in_rand_bsts(5, 20, 1000, 0, 1000000)
+    for k, v in d.iteritems():
+        print('probability of linearity in rbsts with %d nodes = %f' % (k, v[0]))
+
+def unit_test_06(from_num_nodes, upto_num_nodes):
+    d = estimate_list_probs_in_rand_bsts(from_num_nodes, upto_num_nodes, 1000, 0, 1000000)
+    for k, v in d.iteritems():
+        print('probability of linearity in rbsts with %d nodes = %f' % (k, v[0]))
+
+def unit_test_07(num_nodes_start, num_nodes_end):
+    d = estimate_balance_probs_in_rand_bsts(num_nodes_start, num_nodes_end, 1000, 0, 1000000)
+    for k, v in d.iteritems():
+        print('probability of balance in rbsts with %d nodes = %f' % (k, v[0]))
+
+
+
+    
+
+
+
+
